a7bb882f4502beadd6c79f8abee33fd89eb856f6
chrisoney <christopher.oney@yahoo.com>
2022-02-20 15:10:16 -0800

    add more content

--- chat_scan/main.js
+++ chat_scan/main.js
@@ -1,3 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ list: [] });
   console.log('Extension running.');
 });
--- chat_scan/popup.js
+++ chat_scan/popup.js
@@ -1,19 +1,33 @@
-let removeUser = document.getElementById("removeUser");
+let addToListButton = document.getElementById("addToList");
 
 // chrome.storage.sync.get("color", ({ color }) => {
 //   changeColor.style.backgroundColor = color;
 // });
 
-removeUser.addEventListener("click", async () => {
+addToListButton.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: removeUserFromChat,
+    function: addToList,
   });
 });
 
-const removeUserFromChat = () => {
+const addToList = () => {
+  const input = document.getElementById('usernameInput');
+  console.log(addToListButton)
+  if (input.value === '') return;
+  chrome.storage.sync.get("list", ({ list }) => {
+    if (!list) list = [];
+    chrome.storage.sync.set({ list: [...list, input.value] });
+    const listEle = document.querySelector('.userList')
+    const listItem = document.createElement('li');
+    listItem.innerText = input.value;
+    listEle.appendChild(listItem);
+  });
+}
+
+const removeUsersFromChat = () => {
   const chatWindow = document.body.querySelector("[data-test-selector='chat-scrollable-area__message-container']");
   const chatMessages = chatWindow.querySelectorAll('.chat-line__message');
   chatMessages.forEach((msg) => {
