c0b1bb92f238bde63e848f2018c4408402b69158
chrisoney <christopher.oney@yahoo.com>
2022-02-25 16:51:27 -0800

    clean out backgrund

--- storage_testing/background.js
+++ storage_testing/background.js
@@ -1,7 +1,3 @@
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
   chrome.storage.sync.set({ list: [] });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
 });
